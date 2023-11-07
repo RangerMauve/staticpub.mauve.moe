d390201ef28bdf254fc1d6b3e5d81915eb310f8c
Mauve Signweaver <contact@mauve.moe>
2023-11-07 13:54:20 -0500

    Add http signature code

--- notify_followers.js
+++ notify_followers.js
@@ -1,18 +1,29 @@
+import { fetch } from 'http-signed-fetch'
+
 import { readFile } from 'node:fs/promises'
 
 const domain = 'staticpub.mauve.moe'
 const username = 'mauve'
+const publicKeyId = `https://${domain}/about.jsonld#main-key`
+
+const url = `https://social.dp.chanterelle.xyz/v1/@${username}@${domain}/inbox`
+
+const keypair = JSON.parse(await readFile('keypair.json', 'utf8'))
+
 const post = 'newpost.jsonld'
 const actor = 'about.jsonld'
-const outbox = `https://social.dp.chanterelle.xyz/v1/@${username}@${domain}/outbox`
 
 const postContent = JSON.parse(await readFile(post, 'utf8'))
 const actorData = JSON.parse(await readFile(actor, 'utf8'))
 
-const response = await fetch(outbox, {
+const response = await fetch(url, {
+  keypair,
+  publicKeyId,
   method: 'post',
   headers: {
-    'Content-Type': 'application/ld+json'
+    'Content-Type': 'application/ld+json',
+    date: new Date().toUTCString(),
+    host: new URL(url).host
   },
   body: JSON.stringify(
     makeActivity(
